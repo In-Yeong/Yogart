fe740a9649dcf26b09443d9d3f658b79606509e3
SH <seunghee6022@gmail.com>
2020-08-05 15:08:26 +0900

    UPDATE | yoga poses category by difficuly

--- frontend/yogart/src/views/coaching/YogaPosePage.vue
+++ frontend/yogart/src/views/coaching/YogaPosePage.vue
@@ -1,9 +1,10 @@
 <template>
-  <div class="container">
-      <h1>요가 포즈 페이지 입니다</h1>
+  <div class="container my-5" >
+      <h1 class="my-5">Making Yoga Pose List</h1>
+        <h5>Wish poses :</h5>
         <p>{{poseList}}</p>
        <button @click="makeList" class="w3-btn w3-round-xlarge w3-red w3-large m-3">make pose list</button>
-      <button class="w3-btn w3-round-xlarge w3-black w3-large">Put into my list</button>
+      <!-- <button class="w3-btn w3-round-xlarge w3-black w3-large">Put into my list</button> -->
        <div class="d-flex flex-column">
             <div class="w3-bar" >
             <!-- <button class="w3-bar-item w3-button w3-black" @click="onDiary">다이어리</button> -->
@@ -12,42 +13,55 @@
             <button class="w3-bar-item w3-button w3-teal" style="width:150px;" @click="intermediateBtn">intermediate</button>
             <button class="w3-bar-item w3-button w3-red" style="width:150px;"  @click="expertBtn">expert</button>
             </div>
-            <div class="schedule">
+            <div class="pose-box">
             
-                <!-- <div v-if="all"><YogaPoseAll :posefiles="posefiles"/></div>
-                <div v-if="beginner"><YogaPoseBeginner  :posefiles="beginnerPosefiles"/></div>
-                <div v-if="intermediate"><YogaPoseIntermediate  :posefiles="intermediatePosefiles"/></div>
-                <div v-if="expert"><YogaPoseExpert :posefiles="expertPosefiles"/></div>
-             -->
+                <div v-if="all">
+                    <div class="row">
+                    <div class="col-sm-3" id="poses" v-for="posefile in posefiles" :key="posefile.pose_name">  
+                        <img @click="poseChoose(posefile)" class="user-profile m-3" :src="require(`../../../public/photos/${posefile.file_reference}`)">
+                        <p>{{posefile.pose_name}}</p>
+                    </div>      
+                    </div>
+                </div>
+                <div v-if="beginner">
+                    <div class="row">
+                    <div class="col-sm-3" id="poses" v-for="posefile in beginnerPosefiles" :key="posefile.pose_name">  
+                        <img @click="poseChoose(posefile)" class="user-profile m-3" :src="require(`../../../public/photos/${posefile.file_reference}`)">
+                        <p>{{posefile.pose_name}}</p>
+                    </div>      
+                    </div>
+                </div>
+                <div v-if="intermediate">
+                    <div class="row">
+                    <div class="col-sm-3" id="poses" v-for="posefile in intermediatePosefiles" :key="posefile.pose_name">  
+                        <img @click="poseChoose(posefile)" class="user-profile m-3" :src="require(`../../../public/photos/${posefile.file_reference}`)">
+                        <p>{{posefile.pose_name}}</p>
+                    </div>      
+                    </div>
+                </div>
+                <div v-if="expert">
+                    <div class="row">
+                    <div class="col-sm-3" id="poses" v-for="posefile in expertPosefiles" :key="posefile.pose_name">  
+                        <img @click="poseChoose(posefile)" class="user-profile m-3" :src="require(`../../../public/photos/${posefile.file_reference}`)">
+                        <p>{{posefile.pose_name}}</p>
+                    </div>      
+                    </div>
+                </div>
+              
+            
             </div>
         </div>
 
-      
-      <div class="row">
-        <div class="col-sm-3" id="poses" v-for="posefile in tempPosefiles" :key="posefile.pose_name">
-            
-                <img @click="poseChoose(posefile)" class="user-profile m-3" :src="require(`../../public/photos/${posefile.file_reference}`)">
-                <p>{{posefile.pose_name}}</p>
-       </div>      
-      </div>
   </div>
 </template>
 
 <script>
-import posefiles from '../../public/json.js'
-// import YogaPoseAll from '../components/YogaPose/YogaPoseAll.vue'
-// import YogaPoseBeginner from '../components/YogaPose/YogaPoseBeginner.vue'
-// import YogaPoseIntermediate from '../components/YogaPose/YogaPoseIntermediate.vue'
-// import YogaPoseExpert from '../components/YogaPose/YogaPoseExpert.vue'
+import posefiles from '../../../public/json.js'
+
 // console.log(posefiles)
 export default {
     name : 'YogaPosePage',
-    components : {
-        // YogaPoseAll,
-        // YogaPoseBeginner,
-        // YogaPoseIntermediate,
-        // YogaPoseExpert
-    },
+
     data() {
         return {
             tempPosefiles : posefiles,
@@ -67,31 +81,32 @@ export default {
             
         }
     },
-    created() {
-        // this.difficultyDistribution()
-       },
+    mounted() {
+        this.difficultyDistribution()
+    },
     methods : {
-        // difficultyDistribution() {
-        //     this.posefiles.forEach(function(posefile) {
-        //     console.log(posefile.difficulty)
-        //     if (posefile.difficulty === 'Beginner') {
-        //             console.log(posefile.difficulty)
-        //             this.beginnerPosefiles.push(posefile)
-        //         }
-        //         else if (posefile.difficulty === 'Intermediate') {
-        //             this.intermediatePosefiles.push(posefile)
-        //         }
-        //         else if (posefile.difficulty === 'Expert') {
-        //             console.log("gg")
-        //             this.expertPosefiles.push(posefile)
-        //             console.log(this.expertPosefiles)
-        //         }
-        // })
-        // },
+        difficultyDistribution() {
+            this.posefiles.forEach(function(posefile) {
+            console.log(posefile.difficulty)
+            if (posefile.difficulty === 'Beginner') {
+                    console.log(posefile.difficulty)
+                    this.beginnerPosefiles.push(posefile)
+                }
+                else if (posefile.difficulty === 'Intermediate') {
+                    this.intermediatePosefiles.push(posefile)
+                }
+                else if (posefile.difficulty === 'Expert') {
+                    console.log(this)
+                    console.log(this.expertPosefiles)
+                    this.expertPosefiles.push(posefile)
+                    
+                }
+        }.bind(this))
+        },
         poseChoose(btnPose) {
             if (this.poseList.length <= this.max ) {
                 if (this.poseList.includes(btnPose.pose_name)){
-                    // this.poseList.filter(function(value, index, arr){ return value > 5;})
+
                     var idx = this.poseList.indexOf(btnPose.pose_name)
                     this.poseList.splice(idx,1);
                 }
@@ -136,11 +151,12 @@ export default {
         },
         expertBtn() {
             console.log('expert버튼을 눌렀습니다.')
-           this.all = false
-           this.beginner = false
-           this.intermediate = false
-           this.expert = true
-           console.log(this.all,this.beginner,this.intermediate,this.expert)  
+            
+            this.all = false
+            this.beginner = false
+            this.intermediate = false
+            this.expert = true
+            console.log(this.all,this.beginner,this.intermediate,this.expert)  
         },
     }
 }
@@ -160,5 +176,8 @@ export default {
   background-repeat: no-repeat;
   background-position: center center;
   background-size: cover;
+}
+.pose-box {
+    background-color: lightgray;
 }
 </style>
