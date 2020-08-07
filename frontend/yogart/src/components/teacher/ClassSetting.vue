5f4069b9a604b1218ae2da153c2fa9d4ddfd16f4
조규성 <pandora_h@naver.com>
2020-08-07 16:48:32 +0900

    UPDATE | art

--- frontend/yogart/src/components/teacher/ClassSetting.vue
+++ frontend/yogart/src/components/teacher/ClassSetting.vue
@@ -5,6 +5,7 @@
         <br>
         <label for="class_price">회당 가격</label>
         <input type="text" id="class_price">
+        <!-- 데이터 피커를 이용해 날짜를 받으면, 그 날짜에 열리는 모든 수업을 리스팅한다. -->
         <DatePicker></DatePicker>
     </div>
 </template>
