032b1c55e2242103b133737c05381ae8d696f405
SH <seunghee6022@gmail.com>
2020-08-08 15:25:28 +0900

    UPDATE | add pictures into Yoga List Page

--- frontend/yogart/src/components/YogaPoseList/YogartRecommend.vue
+++ frontend/yogart/src/components/YogaPoseList/YogartRecommend.vue
@@ -3,8 +3,8 @@
     <div>
         <h1 class="m-5">요가트 추천 코스</h1>
         <div class="row">
-            <div class="col-4 border rect" @click="clickSet(1)">Relexing Body</div>
-            <div class="col-4 border rect" @click="clickSet(2)">Energy Morning</div>
+            <div class="col-4 border rect d-flex flex-column" @click="clickSet(1)"><img style="width:450px; height:300px;" :src="require('../../../public/yogaList/relax.jpg')"/><p>Relexing Body</p></div>
+            <div class="col-4 border rect d-flex flex-column" @click="clickSet(2)"><img style="width:450px; height:300px;" :src="require('../../../public/yogaList/energy.jpg')"/><p>Energy Morning</p></div>
         </div>
     </div>
 </template>
