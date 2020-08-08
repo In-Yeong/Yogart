8ca61ba499ef869f36acdd77ad5e3b438718bac3
SH <seunghee6022@gmail.com>
2020-08-08 13:47:37 +0900

    UPDATE | data cleansing | #S03P13D202-54

--- frontend/yogart/src/views/coaching/YogaPosePage.vue
+++ frontend/yogart/src/views/coaching/YogaPosePage.vue
@@ -1,65 +1,75 @@
 <template>
   <div class="container my-5" >
-      <h1 class="my-5">Making Yoga Pose List</h1>
-        <h5>Course Name</h5>
+      <h1 class="my-5">나만의 코스 만들기</h1>
+        <h5>코스명</h5>
         <span>
             <input class="w-25" v-model="courseName">
-            <button @click="makeList(courseName)" class="w3-btn w3-round-xlarge w3-red w3-small m-3">make pose list</button>
+            <button @click="makeList(courseName)" class="w3-btn w3-round-xlarge w3-red w3-small m-3">생성</button>
         </span>
-        <h5>Wish poses :</h5>
+        <h5>선택한 포즈 :</h5>
         <p>{{poseList}}</p>
         <!-- <p>{{poseIndexList}}</p> -->
        <div class="d-flex flex-column">
             <div class="w3-bar" >
             <!-- <button class="w3-bar-item w3-button w3-black" @click="onDiary">다이어리</button> -->
-            <button class="w3-bar-item w3-button w3-black" style="width:150px;" @click="allBtn">All</button>
-            <button class="w3-bar-item w3-button w3-gray" style="width:150px;" @click="beginnerBtn">beginner</button>
-            <button class="w3-bar-item w3-button w3-teal" style="width:150px;" @click="intermediateBtn">intermediate</button>
-            <button class="w3-bar-item w3-button w3-red" style="width:150px;"  @click="expertBtn">expert</button>
+            <button class="w3-bar-item w3-button w3-black" style="width:150px;" @click="allBtn">전체</button>
+            <button class="w3-bar-item w3-button w3-gray" style="width:150px;" @click="beginnerBtn">초급</button>
+            <button class="w3-bar-item w3-button w3-teal" style="width:150px;" @click="intermediateBtn">중급</button>
+            <button class="w3-bar-item w3-button w3-red" style="width:150px;"  @click="expertBtn">고급</button>
             </div>
             <div class="pose-box">
             
                 <div v-if="all">
                     <div class="row">
+                       
                     <div class="col-sm-3" id="poses" v-for="posefile in posefiles" :key="posefile.pose_name">  
                        <div class="pose-img">
                         <img @click="poseChoose(posefile)" class="user-profile m-3" :src="require(`../../../public/photos/${posefile.file_reference}`)">
                         <img @click="poseChoose(posefile)" class="user-profile m-3 pose-img-top" :src="require('../../../public/css/heart.png')">
                         </div> 
-                        <p>{{posefile.pose_name}}</p>
+                        <p>{{posefile.korean_pose_name}}</p>
+                        <!-- <p>{{posefile.id}}</p> -->
                     </div>      
                     </div>
                 </div>
                 <div v-if="beginner">
                     <div class="row">
+                       
                     <div class="col-sm-3" id="poses" v-for="posefile in beginnerPosefiles" :key="posefile.pose_name">  
                          <div class="pose-img">
                             <img @click="poseChoose(posefile)" class="user-profile m-3" :src="require(`../../../public/photos/${posefile.file_reference}`)">
                             <img @click="poseChoose(posefile)" class="user-profile m-3 pose-img-top" :src="require('../../../public/css/heart.png')">
                         </div> 
-                        <p>{{posefile.pose_name}}</p>
+                        <p>{{posefile.korean_pose_name}}</p>
+                        <!-- <p>{{posefile.id}}</p> -->
                     </div>      
                     </div>
                 </div>
                 <div v-if="intermediate">
                     <div class="row">
+                       
                     <div class="col-sm-3" id="poses" v-for="posefile in intermediatePosefiles" :key="posefile.pose_name">  
+                        
                          <div class="pose-img">
                             <img @click="poseChoose(posefile)" class="user-profile m-3" :src="require(`../../../public/photos/${posefile.file_reference}`)">
                             <img @click="poseChoose(posefile)" class="user-profile m-3 pose-img-top" :src="require('../../../public/css/heart.png')">
                         </div> 
-                        <p>{{posefile.pose_name}}</p>
+                        <p>{{posefile.korean_pose_name}}</p>
+                        <!-- <p>{{posefile.id}}</p> -->
                     </div>      
                     </div>
                 </div>
                 <div v-if="expert">
                     <div class="row">
+                       
                     <div class="col-sm-3" id="poses" v-for="posefile in expertPosefiles" :key="posefile.pose_name">  
+                        
                          <div class="pose-img">
                             <img @click="poseChoose(posefile)" class="user-profile m-3" :src="require(`../../../public/photos/${posefile.file_reference}`)">
                             <img @click="poseChoose(posefile)" class="user-profile m-3 pose-img-top" :src="require('../../../public/css/heart.png')">
                         </div> 
-                        <p>{{posefile.pose_name}}</p>
+                        <p>{{posefile.korean_pose_name}}</p>
+                        <!-- <p>{{posefile.id}}</p> -->
                     </div>      
                 </div>
             </div>  
@@ -120,14 +130,14 @@ export default {
         },
         poseChoose(btnPose) {
             if (this.poseList.length < this.max ) {
-                if (this.poseList.includes(btnPose.pose_name)){
+                if (this.poseList.includes(btnPose.korean_pose_name)){
 
-                    var idx = this.poseList.indexOf(btnPose.pose_name)
+                    var idx = this.poseList.indexOf(btnPose.korean_pose_name)
                     this.poseList.splice(idx,1);
                     this.poseIndexList.splice(idx,1);
                 }
                 else {
-                    this.poseList.push(btnPose.pose_name)
+                    this.poseList.push(btnPose.korean_pose_name)
                     this.poseIndexList.push(btnPose.id)
                 }
             }
