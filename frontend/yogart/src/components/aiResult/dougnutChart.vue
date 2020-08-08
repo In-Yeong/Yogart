5cc46fb6fb31cc1a9179e35ee873a02d4b2f07a5
SH <seunghee6022@gmail.com>
2020-08-08 12:06:35 +0900

    UPDATE | dividing result page charts to two components

---
+++ frontend/yogart/src/components/aiResult/dougnutChart.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+      <canvas id="dougnutChart"></canvas>
+  </div>
+</template>
+
+<script>
+export default {
+    name : 'dougnutChart',
+    props: {
+        dougnutdata : Array,
+    },
+    mounted() {
+        this.createChart()
+    },
+    methods : {
+        createChart() {
+             var ctx2 = document.getElementById('dougnutChart').getContext('2d');
+            var dougnutChart = new Chart(ctx2, {
+                // The type of chart we want to create
+                type: 'doughnut',
+
+                // The data for our dataset
+                data: {
+                    labels: ['다리', '척추', '복근', '전신', '에너지','릴렉스'],
+                    datasets: [{
+                        label: '부위 포함',
+                        hoverBackgroundColor : '#8e6b73',
+                        data: [1,4,3,2,1,1],
+                        backgroundColor: [
+                            '#8e6b73',
+                            '#a94300',
+                            '#ffb000',
+                            '#ffdf00',
+                            '#5b9b37',
+                            '#153d02',            
+                            
+                        ]
+                    
+                    }]      
+                
+                },
+
+                options: {
+                    legend: {
+                        labels: {
+                            // This more specific font property overrides the global property
+                            fontColor: 'black',
+                            fontSize : 20,
+                            
+                            
+                        },
+
+                    },
+                    title : {
+                        display : true,
+                        fontSize : 25,
+                        text : '부위별 비율'
+                    } ,
+                    plugins: {
+                    
+                        labels: [
+                            {
+                            fontColor : 'black',
+                            fontSize : 20,
+                            render: 'label',
+                            position: 'outside'
+                            },
+                            {
+                            fontColor : 'black',
+                            fontSize : 20,
+                            render: 'percentage'
+                            }
+                        ]
+                    }
+                
+                }
+
+            });
+        }
+    }
+}
+</script>
+
+<style>
+#dougnutChart {
+    height : 400px !important;
+    width : 800px !important;
+    margin : 0;
+}
+</style>
--- frontend/yogart/src/views/coaching/AICoachingResultPage.vue
+++ frontend/yogart/src/views/coaching/AICoachingResultPage.vue
@@ -3,9 +3,8 @@
     <h2 class="m-3">AI 코칭 결과</h2>
     <h5>총 소요시간  |    {{totalTime}}</h5>
     <div class="d-flex mx-auto pr-0">
-        <canvas id="lineChart"></canvas>
-        <canvas id="dougnutChart"></canvas>
-   
+        <lineChart :lineLabels="lineLabels" :linedata="linedata"/>
+        <dougnutChart :dougnutdata="dougnutdata"/>
     </div>
 </div>
 </template>
@@ -13,8 +12,15 @@
 <script>
 import posefiles from '../../../public/json.js'
 import 'chartjs-plugin-labels'
+import lineChart from '../../components/aiResult/lineChart.vue'
+import dougnutChart from '../../components/aiResult/dougnutChart.vue'
+
 export default {
     name : 'AICoachingResultPage',
+    components : {
+        lineChart,
+        dougnutChart,
+    },
     data() {
         return {
             posefiles : posefiles,
@@ -40,183 +46,10 @@ export default {
     created(){
          this.createLineLabels()
     },
-    mounted() {
-       
-
-
-        var ctx = document.getElementById('lineChart').getContext('2d');
-        var gradientStroke = ctx.createLinearGradient(500, 0, 100, 0);
-        gradientStroke.addColorStop(0, "#80b6f4");
-        gradientStroke.addColorStop(1, "#f49080");
-        var gradientFill = ctx.createLinearGradient(500, 0, 100, 0);
-        gradientFill.addColorStop(0, "rgba(128, 182, 244, 0.6)");
-        gradientFill.addColorStop(1, "rgba(244, 144, 128, 0.6)");
-        // gradientFill.addColorStop(0, "#80b6f4");
-        // gradientFill.addColorStop(1, "f#49080");
-        var lineChart = new Chart(ctx, {
-        //  multiTooltipTemplate: "<%= datasetLabel %> - <%= value %>",
-            // The type of chart we want to create
-            type: 'line',
-
-            // The data for our dataset
-            data: {
-                labels: this.lineLabels,
-                datasets: [{
-                    
-                    label: '소요 시간(초)',
-                    // backgroundColor: 'rgb(255, 99, 132)',
-                    // borderColor: '#3c5b74',
-                    // hoverBackgroundColor : '#3c5b74',
-                    data: [30, 40, 50, 32, 44, 35, 30],
-                    borderColor: gradientStroke,
-                    pointBorderColor: gradientStroke,
-                    pointBackgroundColor: gradientStroke,
-                    pointHoverBackgroundColor: gradientStroke,
-                    pointHoverBorderColor: gradientStroke,
-                    pointBorderWidth: 10,
-                    pointHoverRadius: 10,
-                    pointHoverBorderWidth: 1,
-                    pointRadius: 3,
-                    fill: true,
-                    backgroundColor: gradientFill,
-                    borderWidth: 4,
-                }]
-            },
-
-            // Configuration options go here
-            options: {
-               scales: {
-                    xAxes: [{
-                    scaleLabel: {
-                        fontWeight : 'bold',
-                        fontSize : 20,
-                        display: true,
-                        labelString: '요가 동작'
-                    }
-                    }],
-                    yAxes: [{
-                    scaleLabel: {
-                        fontWeight : 'bold',
-                        fontSize : 20,
-                        display: true,
-                        labelString: '소요시간(초)'
-                    }
-                    }]
-                } ,    
-                legend: {
-                    labels: {
-                        // This more specific font property overrides the global property
-                        fontColor: 'black',
-                        fontSize : 20,
-                    },
-
-                },
-                title : {
-                    display : true,
-                    fontSize : 30,
-                    text : '동작 별 소요시간'
-                } ,
-                scaleLabel : {
-                    display : true,
-                },
-                plugins: {
-                    datalabels: {
-                        color: 'blue',
-                        labels: {
-                          
-                            title: {
-                                font: {
-                                    weight: 'bold'
-                                }
-                            },
-                            value: {
-                                color: 'green'
-                            }
-                        }
-                    }
-                }             
-            }
-
-        });
-   
-        var ctx2 = document.getElementById('dougnutChart').getContext('2d');
-        var dougnutChart = new Chart(ctx2, {
-            // The type of chart we want to create
-            type: 'doughnut',
-
-            // The data for our dataset
-            data: {
-                labels: ['다리', '척추', '복근', '전신', '에너지','릴렉스'],
-                datasets: [{
-                    label: '부위 포함',
-                    hoverBackgroundColor : '#8e6b73',
-                    data: [1,4,3,2,1,1],
-                    backgroundColor: [
-                        '#8e6b73',
-                        '#a94300',
-                        '#ffb000',
-                        '#ffdf00',
-                        '#5b9b37',
-                        '#153d02',            
-                        
-                    ]
-                   
-                }]      
-            
-            },
-            
-
-            // Configuration options go here
-            options: {
-                legend: {
-                    labels: {
-                        // This more specific font property overrides the global property
-                        fontColor: 'black',
-                        fontSize : 20,
-                        
-                        
-                    },
-
-                },
-                title : {
-                    display : true,
-                    fontSize : 30,
-                    text : '부위별 비율'
-                } ,
-                plugins: {
-                  
-                    labels: [
-                        {
-                        fontColor : 'black',
-                        fontSize : 20,
-                        render: 'label',
-                        position: 'outside'
-                        },
-                        {
-                        fontColor : 'black',
-                        fontSize : 20,
-                        render: 'percentage'
-                        }
-                    ]
-                }
-               
-            }
-
-        });
-        
-    },
+ 
 }
 </script>
 
 <style >
-#lineChart {
-    height : 400px !important;
-    width : 700px !important;
-    margin-left : 50px;
-}
-#dougnutChart {
-    height : 400px !important;
-    width : 800px !important;
-    margin : 0;
-}
+
 </style>
