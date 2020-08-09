b92e628371de515cf4d3eb6177fc46f28085603f
박인영 <in0_2015@naver.com>
2020-08-10 06:37:28 +0900

    UPDATE | user design list back connecting

--- frontend/yogart/src/components/YogaPoseList/UserDesign.vue
+++ frontend/yogart/src/components/YogaPoseList/UserDesign.vue
@@ -19,10 +19,13 @@
             }
         },
         created() {
-            
+            this.getUserCourse()
         },
         methods: {
-            clickSet(num) {
+            clickSet() {
+                console.log('here~~~')
+                var num = this.$cookies.get('clickId')*1
+                console.log(num)
                 this.$emit("clickSet",num)
             },
             newCourse() {
@@ -34,10 +37,14 @@
                     console.log(res)
                     res.data.forEach(e => {
                         var list = document.createElement('div');
-                        list.classList.add = 'col-4 border rect d-flex flex-column'
-                        list.click = `clickSet(${e.listId})`
+                        list.classList.add('col-4', 'border', 'rect', 'd-flex', 'flex-column');
+                        list.addEventListener('click', function(){
+                            // 미완 (테스트 필요)
+                            this.$cookies.set('clickId', e.listId)
+                            this.clickSet
+                        })
                         list.innerText = e.courseName
-                        document.getElementById('mylist').appendChild(list)
+                        document.getElementById('mylist').prepend(list)
                     });
 
                 })
