b648d002e1a5601993cd49bcd8fd2d462cbd11e7
SH <seunghee6022@gmail.com>
2020-08-11 20:38:13 +0900

    UPDATE | PT Management component

--- frontend/yogart/src/components/mypage/PTManagement.vue
+++ frontend/yogart/src/components/mypage/PTManagement.vue
@@ -8,12 +8,78 @@
     <div class="my-3">
       <div v-if="today">
         <h5>오늘 진행 PT</h5>
+        <table class="table">
+        <thead class="thead-dark">
+          <tr>
+            <!-- <th scope="col">#</th> -->
+            <th scope="col">강사명</th>
+            <th scope="col">수업명</th>
+            <th scope="col">수업일시</th>
+            <th scope="col">바로가기</th>
+          </tr>
+        </thead>
+        <tbody>
+            <div v-for="todayCourse in todayCourses" :key="todayCourse.courseID">{{}} | 
+              <tr>
+                <!-- <th scope="row">1</th> -->
+                <td>{{todayCourse.teacherName}}</td>
+                <td>{{todayCourse.courseName}}</td>
+                <td>{{todayCourse.courseDate}}</td>
+                <td><button class="w3-bar-item w3-button w3-red">입장하기</button></td>
+              </tr>
+            </div>
+        </tbody>
+      </table>
       </div>
       <div v-if="past">
         <h5>과거 PT 내역</h5>
+        <table class="table">
+        <thead class="thead-dark">
+          <tr>
+            <!-- <th scope="col">#</th> -->
+            <th scope="col">강사명</th>
+            <th scope="col">수업명</th>
+            <th scope="col">수업일시</th>
+            <th scope="col">출석현황</th>
+            <th scope="col">리뷰</th>
+          </tr>
+        </thead>
+        <tbody>
+            <div v-for="pastCourse in pastCourses" :key="pastCourse.courseID">{{}} | 
+              <tr>
+                <!-- <th scope="row">1</th> -->
+                <td>{{pastCourse.teacherName}}</td>
+                <td>{{pastCourse.courseName}}</td>
+                <td>{{pastCourse.courseDate}}</td>
+                <td>{{pastCourse.isAttend}}</td>
+                <td><button class="w3-bar-item w3-button w3-red">리뷰작성</button></td>
+              </tr>
+            </div>
+        </tbody>
+      </table>
       </div>
       <div v-if="future">
-        <h5>진행 예정인 PT</h5>
+        <h5>진행 예정 PT</h5>
+            <table class="table">
+        <thead class="thead-dark">
+          <tr>
+            <!-- <th scope="col">#</th> -->
+            <th scope="col">강사명</th>
+            <th scope="col">수업명</th>
+            <th scope="col">수업일시</th>
+          </tr>
+        </thead>
+        <tbody>
+            <div v-for="futureCourse in futureCourses" :key="futureCourse.courseID">{{}} | 
+              <tr>
+                <!-- <th scope="row">1</th> -->
+                <td>{{futureCourse.teacherName}}</td>
+                <td>{{futureCourse.courseName}}</td>
+                <td>{{futureCourse.courseDate}}</td>
+              </tr>
+            </div>
+        </tbody>
+      </table>
       </div> 
     </div>
      
@@ -46,7 +112,7 @@ export default {
     methods : {
       getPT() {
         //오늘 날짜 기준으로 과거,오늘,미래로 나눠서 1:1 PT수업리스트를 받는 함수
-        // user(auth-token을 보내주면 그 유저에 연결된 1:1 pt 수업을 보내주세요 - 내용 : 수업명, 강사명, 출석현황(isAttend),수업날짜(dataTime) 포함된 object의 리스트)
+        // user(auth-token을 보내주면 그 유저에 연결된 1:1 pt 수업을 보내주세요 - 내용 : 수업명, 수업id, 강사명, 출석현황(isAttend),수업날짜(dataTime) 포함된 object의 리스트)
         const RequestHeaders = {
           headers : {
             Authorization : this.$cookies.get('auth-token')
