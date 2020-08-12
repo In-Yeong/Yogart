c84b0985835d7a64d3f850b91962abac497804e6
박인영 <in0_2015@naver.com>
2020-08-12 17:13:11 +0900

    UPDATE | update

--- frontend/yogart/src/components/NavBar.vue
+++ frontend/yogart/src/components/NavBar.vue
@@ -1,143 +1,69 @@
 <template>
-      <!-- 여기 네브바 넣음 -->
-    <!-- Start Navbar Area -->
     <div class="navbar-area" style="margin-bottom=55px;">
-      <!-- Menu For Mobile Device -->
-      <div class="container">
 
-        <div class="mobile-nav">
-          <router-link to="/" class="logo">
-            Logo
-          </router-link>
-        </div>
-      </div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="/">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-      <!-- Menu For Desktop Device -->
-      <div class="main-nav">
-        <div class="container">
-          <nav class="navbar navbar-expand-md navbar-light">
-            <router-link class="navbar-brand" to="/">
-              Logo
-            </router-link>
-            <div class="collapse navbar-collapse mean-menu" id="navbarSupportedContent">
-              <ul class="navbar-nav ml-auto">
-                <!-- <li class="nav-item">
-                  <a href="#" class="nav-link dropdown-toggle active">Service</a>
-                  <ul class="dropdown-menu">
-                    <li class="nav-item">
-                     
-                      <a href="#" class="nav-link">AI Coaching</a>
-                    </li>
-                    <li class="nav-item">
-                      <a href="#" class="nav-link">1:1 Yoga PT</a>
-                    </li>
-                   
-                    </li>
-                  </ul>
-                </li> -->
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
-                  <router-link to="/notice" class="nav-link">Notice</router-link>
-                </li>
-                <!-- 로그인과 사인업 로그인 중에는 안 보이도록 해야함. -> 로그아웃, 마이페이지 출력 -->
-                <template v-if="!isLogin">
-                  <li class="nav-item">
-                    <a @click="openLoginModal" style="cursor:pointer;" class="nav-link">Login</a>
-                  </li>
-                  <li class="nav-item">
-                    <router-link to="/accounts/signup" class="nav-link">Signup</router-link>
-                  </li>
-                </template>
-                <template v-else>
-                  <li class="nav-item">
-                    <router-link to="/mypage" class="nav-link">Mypage</router-link>
-                  </li>
-                  <li class="nav-item">
-                    <a @click="logoutEmmit" style="cursor:pointer;" class="nav-link">Logout</a>
-                  </li>
-                </template>
-
-                <li class="nav-item">
-                  <router-link to="#" class="nav-link dropdown-toggle">Pages</router-link>
-                  <ul class="dropdown-menu">
-                    <li class="nav-item">
-                      <router-link to="#" class="nav-link">About</router-link>
-                    </li>
-                    <li class="nav-item">
-                      <router-link to="#" class="nav-link">Services</router-link>
-                    </li>
-                    <li class="nav-item">
-                      <router-link to="#" class="nav-link">Team</router-link>
-                    </li>
-                    <li class="nav-item">
-                      <router-link to="#" class="nav-link">Testimonials</router-link>
-                    </li>
-                    <li class="nav-item">
-                      <router-link to="#" class="nav-link">Contact</router-link>
-                    </li>
-                  </ul>
+                    <a class="nav-link" href="/coaching/yogaposelist">AI coaching</a>
                 </li>
                 <li class="nav-item">
-                  <router-link to="#" class="nav-link dropdown-toggle">Service</router-link>
-                  <ul class="dropdown-menu">
-                    <li class="nav-item">
-                      <router-link to="#" class="nav-link dropdown-toggle">AI Coaching</router-link>
-                      <ul class="dropdown-menu">
-                        <li class="nav-item">
-                          <router-link to="/coaching" class="nav-link">AI Coaching Page</router-link>
-                        </li>
-                        <li class="nav-item">
-                          <router-link to="/coaching/result" class="nav-link">AI Coaching Result</router-link>
-                        </li>
-                        <li class="nav-item">
-                          <router-link to="/coaching/yogapose" class="nav-link">Yoga Pose</router-link>
-                        </li>
-                        <li class="nav-item">
-                          <router-link to="/coaching/yogaposelist" class="nav-link">Yoga Pose List</router-link>
-                        </li>
-                      </ul>
-                    </li>
-                    <li class="nav-item">
-                      <router-link to="#" class="nav-link dropdown-toggle">1:1 Yoga PT</router-link>
-                      <ul class="dropdown-menu">
-                        <li class="nav-item">
-                          <router-link to="#" class="nav-link">Yoga Teachers</router-link>
-                        </li>
-                        <li class="nav-item">
-                          <router-link to="/class" class="nav-link">Yoga Classes</router-link>
-                        </li>
-                        <li class="nav-item">
-                          <router-link to="#" class="nav-link dropdown-toggle">Submenu 3</router-link>
-                          <ul class="dropdown-menu">
-                            <li class="nav-item">
-                              <router-link to="#" class="nav-link">Item 1</router-link>
-                            </li>
-                            <li class="nav-item">
-                              <router-link to="#" class="nav-link">Item 2</router-link>
-                            </li>
-                            <li class="nav-item">
-                              <router-link to="#" class="nav-link">Item 3</router-link>
-                            </li>
-                          </ul>
-                        </li>
-                      </ul>
-                    </li>
-                    <li class="nav-item">
-                      <router-link to="/qna" class="nav-link">Q&A</router-link>
-                    </li>
-                  </ul>
+                    <a class="nav-link" href="/class">1:1 PT</a>
                 </li>
-              </ul>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    HELP
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="/notice">공지사항</a>
+                        <a class="dropdown-item" href="/qna">Q&A</a>
+                    </div>
+                </li>
+                
+                <li class="nav-item dropdown" v-if="isLogin">
+                    <!-- <img class="user-profile-img m-3" :src="userPic"> -->
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    {{userNickname}}님
+                    </a>
+                    <div v-if="isAdmin" class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="/mypage">마이페이지</a>
+                        <a class="dropdown-item" href="/notice/form">공지작성</a>
+                        <a class="dropdown-item" @click="logoutEmmit">로그아웃</a>
+                    </div>
+                    <div v-else-if="isTeacher" class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="/mypage">마이페이지</a>
+                        <a class="dropdown-item" href="#">수업관리</a>
+                        <a class="dropdown-item" @click="logoutEmmit">로그아웃</a>
+                    </div>
+                    <div v-else class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="/mypage">마이페이지</a>
+                        <a class="dropdown-item" @click="logoutEmmit">로그아웃</a>
+                    </div>
+                </li>
+                <li class="nav-item" v-else>
+                    <a class="nav-link" @click="openLoginModal">로그인</a>
+                </li>
+                </ul>
             </div>
-          </nav>
-        </div>
-      </div>
-    </div>
-    <!-- End Navbar Area -->
-    <!-- 네브바 끝 -->
+        </nav>
+    </div> 
+
 </template>
 
 <script>
 export default {
+    name: 'NavBar',
+    data() {
+        return {
+            userNickname: this.$cookies.get('userNickname'),
+            userPic: "http://localhost:8000/api/users/profileImage?authToken=" + this.$cookies.get('auth-token'),
+        }
+    },
     methods: {
         openLoginModal() {
               if (this.isLogin) return
@@ -154,15 +80,15 @@ export default {
 </script>
 
 <style>
-.mobile-nav {
-    background: #000;
-    position: absolute;
-    top: 0;
-    left: 0;
-    padding-top: 15px;
-    padding-bottom: 15px;
-    width: 100%;
-    z-index: 999;
-    height: 99px;
-}
+
+.user-profile-img {
+  display: inline-block;
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+} 
 </style>
