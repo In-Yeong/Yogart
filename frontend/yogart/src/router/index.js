b42d0cd37c6b5b2ac2fee6c67bc0fcbb3ea89cfa
SH <seunghee6022@gmail.com>
2020-08-15 17:26:50 +0900

    UPDATE | designed class list page

--- frontend/yogart/src/router/index.js
+++ frontend/yogart/src/router/index.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import axios from 'axios'
 import store from '../store'
 import Home from '../views/Home.vue'
 import SignupView from '../views/accounts/SignupView.vue'
 import GraphView from '../views/mypage/GraphView.vue'
 import Callback from '../components/sociallogin/naverLogin.vue'
-import QnaView from '../views/QnA/QnaView.vue'
-import QnaCreate from '../views/QnA/QnaCreate.vue'
+import QnaView from '../views/qna/QnaView.vue'
+import QnaCreate from '../views/qna/QnaCreate.vue'
 import NoticeListView from '../views/notice/NoticeListView.vue'
 import NoticeDetailView from '../views/notice/NoticeDetailView.vue'
 import NoticeFormView from '../views/notice/NoticeFormView.vue'
@@ -39,17 +40,55 @@ const requireAuth = () => (from, to, next) => {
 const requireUNAuth = () => (from, to, next) => {
     // console.log(store.state.isLogin)
     if (!store.state.isLogin) return next()
-    return
+    return next('/')
+}
+
+const requireTeacher = () => (from, to, next) => {
+    // 해당 유저가 강사자격을 보유했는지 확인합니다.
+    const requestHeaders = {
+        headers: {
+            Authorization: this.$cookies.get('auth-token')
+        }
+    }
+    axios.get(store.state.SERVER_URL + '/api/users/isTeacher', requestHeaders)
+    .then(res => {
+        if (res.data.isTeacher) {
+            return next()
+        }
+    })
+    .catch(err => {
+        console.error(err)
+        next(false)
+    })
 }
 
 const requireAdmin = () => (from, to, next) => {
-    
+    const requestHeaders = {
+        headers: {
+            Authorization: this.$cookies.get('auth-token')
+        }
+    }
+    axios.get(store.state.SERVER_URL + '/api/users/isAdmin', requestHeaders)
+    .then(res => {
+        if (res.data.isAdmin) {
+            return next()
+        }
+    })
+    .catch(err => {
+        console.error(err)
+        next(false)
+    })
 }
 
 Vue.use(VueRouter)
 
 const routes = [
-    // 카카오 페이 callback
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    // Kakao pay
     {
         path: '/test',
         name: 'test',
@@ -70,21 +109,31 @@ const routes = [
         name: 'PayFail',
         component: PayFail
     },
+    // Spoon
     {
         path: '/spoons',
         name: 'SpoonPurchase',
         component: SpoonPurchase,
         beforeEnter: requireAuth()
     },
+    // All class
+    {
+        path: '/class',
+        name: 'ClassList',
+        component: ClassList
+    },
+    // Teacher
     {
         path: '/teacherpage',
         name: 'TeacherPage',
-        component: TeacherPage
+        component: TeacherPage,
+        beforeEnter: requireTeacher()
     },
     {
         path: '/teachers/class-setting',
         name: 'ClassSetting',
         component: ClassSetting,
+        beforeEnter: requireTeacher()
     },
     {
         path: '/teachers',
@@ -99,18 +148,15 @@ const routes = [
     {
         path: '/teacher-apply',
         name: 'TeacherApply',
-        component: TeacherApply
+        component: TeacherApply,
+        beforeEnter: requireAuth()
     },
     {
         path: '/naver/callback',
         name: 'Callback',
         component: Callback,
     },
-    {
-        path: '/',
-        name: 'Home',
-        component: Home
-    },
+   
     {
         path: '/accounts/signup',
         name: 'SignupView',
@@ -135,6 +181,7 @@ const routes = [
         component: UserUpdate,
         beforeEnter: requireAuth(),
     },
+    // Notice
     {
         path: '/notice',
         name: 'NoticeList',
@@ -143,14 +190,15 @@ const routes = [
     {
         path: '/notice/form',
         name: 'NoticeForm',
-        component: NoticeFormView
-        // 관리자만 접근가능
+        component: NoticeFormView,
+        beforeEnter: requireAdmin()
     },
     {
         path: '/notice/:notice_id',
         name: 'NoticeDetail',
         component: NoticeDetailView
     },
+    // Q&A
     {
         path: '/qna',
         name: 'QnaView',
@@ -162,36 +210,39 @@ const routes = [
         component: QnaCreate,
         beforeEnter: requireAuth(),
     },
+    // AI coaching
     {
         path: '/coaching',
         name: 'AICoachingPage',
-        component: AICoachingPage
-      },
+        component: AICoachingPage,
+        beforeEnter: requireAuth(),
+    },
     {
         path: '/coaching/result',
         name: 'AICoachingResultPage',
-        component: AICoachingResultPage
+        component: AICoachingResultPage,
+        beforeEnter: requireAuth(),
       },
       {
         path: '/coaching/yogaposelist',
         name: 'YogaPoseListPage',
-        component: YogaPoseListPage
+        component: YogaPoseListPage,
+        beforeEnter: requireAuth(),
       },
       {
         path: '/coaching/yogaposelist/detail',
         name: 'YogaPoseListDetailPage',
-        component: YogaPoseListDetailPage
+        component: YogaPoseListDetailPage,
+        beforeEnter: requireAuth(),
       },
       {
         path: '/coaching/yogapose',
         name: 'YogaPosePage',
-        component: YogaPosePage
+        component: YogaPosePage,
+        beforeEnter: requireAuth(),
       },
-      {
-        path: '/class',
-        name: 'ClassList',
-        component: ClassList
-      },
+      // Admin
+      
 ]
 
     const router = new VueRouter({
--- frontend/yogart/src/views/ClassList.vue
+++ frontend/yogart/src/views/ClassList.vue
@@ -1,23 +1,38 @@
 <template>
     <div class="padding-for-nav">
-        <div class="page-index">
-            <div class="page-name">수업 목록</div>
+        <div class="page-index d-flex justify-content-between">
+        <div class="page-name">수업 목록</div>
 
-            <div class="page-select">
+        <div class="form-group">
+            <select class="form-control" id="sel1">
+                <option value="0" @click="resorting('new')"> 최신순</option>
+                <option value="1" @click="resorting('low')">낮은가격순</option>
+                <option value="2" @click="resorting('high')">높은가격순</option>
+            </select>
+        </div>
+
+            <!-- <div class="page-select">
                 <div class="option option-active" id="new" @click="resorting('new')">최신순</div>
                 <div class="option" id="low" @click="resorting('low')">낮은가격순</div>
                 <div class="option" id="high" @click="resorting('high')">높은가격순</div>
-            </div>
+            </div> -->
         </div>
         
-        <div class="class-box row align-items-center" @click="btnClick(yogaClass.id)" v-for="yogaClass in yogaList" :key="yogaClass.id">
+        <!-- <div class="box row align-items-center" @click="btnClick(yogaClass.id)" v-for="yogaClass in yogaList" :key="yogaClass.id">
             <div class="d-inline-block col-2">
                 <img class="user-profile m-3" :src="require('@/assets/Hedgehog.jpg')">
             </div>
             <div class="d-inline-block col-10">
-                <div class="h3">{{ yogaClass.name }}</div>
-                <div>{{ yogaClass.teacher_nickName}} | <span class="price">{{ yogaClass.price}}</span> 스푼</div>
+                <div class="h5">{{ yogaClass.name }}</div>
+                <div>{{ yogaClass.teacher_nickName }} | <span class="price"> {{ yogaClass.price}} </span> 스푼</div>
             </div>
+        </div> -->
+        <div class="box px-3 d-flex justify-content-between align-items-center" @click="btnClick(yogaClass.id)" v-for="yogaClass in yogaList" :key="yogaClass.id">
+            <div class="d-flex align-items-center">
+                <img class="user-profile m-3" :src="require('@/assets/Hedgehog.jpg')">
+                <div class="h5">{{ yogaClass.name }}</div>
+            </div>
+            <div>{{ yogaClass.teacher_nickName }} | <span class="price"> {{ yogaClass.price}} </span> 스푼</div>
         </div>
         <infinite-loading @infinite="infiniteHandler" spinner="waveDots"></infinite-loading>
     </div>
@@ -26,6 +41,7 @@
 <script>
 import InfiniteLoading from 'vue-infinite-loading'
 import axios from 'axios'
+import selectBox from './selectBox'
 
 export default {
     name: 'ClassList',
@@ -66,16 +82,19 @@ export default {
     created() {
         window.scrollTo(0,0);
     },
+    mounted(){
+        this.dropdown()
+    },
     methods: {
-        resorting(str) {
-            var options = document.getElementsByClassName('option')
-            options.forEach(el => {
-                el.classList.remove('option-active')
-            });
-            document.getElementById(str).classList.add('option-active')
-            // axios로 요청보내기
+        // resorting(str) {
+        //     var options = document.getElementsByClassName('option')
+        //     options.forEach(el => {
+        //         el.classList.remove('option-active')
+        //     });
+        //     document.getElementById(str).classList.add('option-active')
+        //     // axios로 요청보내기
 
-        },
+        // },
         btnClick(classId) {
             console.log(classId)
             // 해당 수업 상세 페이지로 이동
@@ -109,9 +128,10 @@ export default {
 <style scoped>
 .page-index {
     height: 8vh;
-    width: 90vh;
+    width: 60%;
     margin: auto;
-    border-bottom: 2px solid rgba(143, 160, 242, 0.3);
+    margin-bottom : 1rem;
+    border-bottom: 2px solid rgba(143, 160, 242, 0.5);
 }
 .page-name {
     float: left;
@@ -166,5 +186,24 @@ export default {
     cursor: pointer;
     background-color: rgba(0, 0, 0, 0.1);
 }
+.box{
+    width :60%;
+    margin : auto;
+    background-color: rgba(255, 255, 255, 0.3);
+}
+.box:hover{
+    cursor:pointer;
+    background-color: rgba(143, 160, 242, 0.2);
+    border : 2px solid rgba(143, 160, 242, 0.5);
+}
 
+option, select#sel1.form-control{
+    width:150px;
+    background-color: rgba(255, 255, 255, 0.3);
+    border : 1px solid rgba(143, 160, 242, 0.5);
+}
+
+select option{
+    background-color: rgba(255, 255, 255, 0.3);
+}
 </style>
