6fb48f336f278fc775a47e81f0328dbfa2fbd398
SH <seunghee6022@gmail.com>
2020-08-18 21:41:27 +0900

    UPDATE | deployed SERVER_URL

--- frontend/yogart/src/components/sociallogin/kakaoLogin.vue
+++ frontend/yogart/src/components/sociallogin/kakaoLogin.vue
@@ -26,7 +26,7 @@ export default {
                 success : function( authObj ) {
                     // console.log( authObj );
                     axios
-                    .post("http://localhost:8000/api/users/kakaoLogin", {
+                    .post(`http://i3d202.p.ssafy.io:8000/api/users/kakaoLogin`, {
                         access_token: authObj.access_token,
                         expires_in: authObj.expires_in,
                         refresh_token: authObj.refresh_token,
