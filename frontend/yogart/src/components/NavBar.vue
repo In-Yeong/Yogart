b2844a75d78bc5de06bfdaaa896448218d4cc41c
조규성 <pandora_h@naver.com>
2020-08-20 16:06:52 +0900

    UPDATE | nav bar link issue

--- frontend/yogart/src/components/NavBar.vue
+++ frontend/yogart/src/components/NavBar.vue
@@ -34,12 +34,13 @@
                     </div>
                     <div v-else-if="isTeacher" class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                         <router-link class="sub-item nav-link" to="/mypage">마이페이지</router-link>
-                        <router-link class="sub-item nav-link" to="/teachers/class-settings">수업관리</router-link>
+                        <router-link class="sub-item nav-link" to="/teacherpage">수업관리</router-link>
                         <a class="sub-item nav-link" @click="logoutEmmit">로그아웃</a>
                     </div>
                     <div v-else class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                         <router-link class="sub-item nav-link" to="/mypage">마이페이지</router-link>
                         <a class="sub-item nav-link" @click="logoutEmmit">로그아웃</a>
+                        <router-link class="sub-item nav-link small-size" to="/teacher-apply" >강사로 활동하기</router-link>
                     </div>
                 </li>
                 <li class="nav-item" v-else>
@@ -214,4 +215,20 @@ export default {
 .sub-item {
     text-align: center;
 }
+
+.small-size {
+    font-size: 14px;
+    color: gray !important;
+    position: relative;
+    padding: 1rem !important;
+    cursor: pointer;
+}
+
+.small-size:after {    
+  content: "";
+  display: block;
+  height: 0px !important;
+  width: 0px !important;
+}
+
 </style>
